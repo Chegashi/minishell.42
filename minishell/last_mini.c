e42e6c3f0b77ec3e3e7e0e1a9fe4ca52c08a1b0b
Nouhaila Bjaghou <nbjaghou@e1r8p6.1337.ma>
2021-06-02 17:32:08 +0100

    last_mini

--- minishell/last_mini.c
+++ minishell/last_mini.c
@@ -1,195 +1,241 @@
-#include<stdio.h>
-#include<signal.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <string.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include "libft/libft.h"
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <curses.h>
-#include <termcap.h>
-#include <sys/ioctl.h>
-#include <term.h>
-#define UP_ARROW 4283163
-#define DOWN_ARROW 4348699
-#define ENTER_ARROW 10
-#define BUFFER_SIZE 2048
-#define CTRLD 4
-typedef struct s_history
-{
-    char *line;
-    struct s_history *prev;
-    struct s_history *next;
-}               t_hist;
-typedef struct s_pipes
-{
-    char *line;
-    char **params;
-	char **file;
-	char **type;
-	int inp;
-	int out;
-    struct s_pipes *next;
-}               t_pipes;
-typedef struct s_cmd{
-    char *line;
-    t_pipes *pipes;
-    struct s_cmd *next;
-}               t_cmd;
+#include "minishell.h"
 
-typedef struct s_shell
-{
-	char **envp;
-    struct s_cmd *cmd;
-	t_hist *hist;
-    int col;
-    char *line;
-}       t_shell;
-
-
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	ft_putstr_fd(str, 1);
 }
-int ft_prompt()
+
+int 	ft_prompt(void)
 {
-    ft_putstr("\e[1;32mminishell$ \e[0;37m");
-    return (1);
+	ft_putstr("\e[1;32mminishell$ \e[0;37m");
+	return (1);
 }
 
-void signal_handler(int signum) 
+void	signal_handler(int signum)
 {
-  	if (signum == SIGINT)
+	if (signum == SIGINT)
 	{
-        ft_putstr("\n");
+		ft_putstr("\n");
 		ft_prompt();
-	} 
-  	else if (signum == SIGQUIT)
+	}
+	else if (signum == SIGQUIT)
 	{
-		ft_putstr("Quit: 3\n");	
-	}  
+		if (g_fork > 0)
+			ft_putstr("Quit: 3\n");
+	}
 }
 
-int putchar_term(int c)
+int 	putchar_term(int c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
+}
+void print_hist(t_shell *shell)
+{
+	t_list *tmp;
+
+	tmp = shell->hist;
+	while (tmp)
+	{
+		ft_putendl_fd(tmp->content, 1);
+		tmp = tmp->next;
+	}
+}
+void	ft_ctrld(t_shell *shell)
+{
+	ft_putstr("exit\n");
+	print_hist(shell);
+	exit(0);
+}
+int	nbr_length(int n)
+{
+	int	i = 0;
+
+	if (n <= 0)
+		i++;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
 }
 
-void ft_ctrld()
+void	get_cursor_position(int *col, int *rows)
 {
-    ft_putstr("exit\n");
-    exit(0);
+	int		a = 0;
+	int		i = 1;
+	char	buf[255];
+	int		ret;
+	int		temp;
+
+	write(0, "\033[6n", 4);  //report cursor location
+	ret = read(0, buf, 254);
+	buf[ret] = '\0';
+	while (buf[i])
+	{
+		if (buf[i] >= '0' && buf[i] <= '9')
+		{
+			if (a == 0)
+				*rows = atoi(&buf[i]) - 1;
+			else
+			{
+				temp = atoi(&buf[i]);
+				*col = temp - 1;
+			}
+			a++;
+			i += nbr_length(temp) - 1;
+		}
+		i++;
+	}
+}
+char *line_hist(t_shell *shell)
+{
+	int i;
+	int j;
+	t_list *tmp;
+
+	i = shell->col;
+	j = 0;
+	tmp = shell->hist;
+	while (tmp)
+	{
+		if (j == i)
+			return(tmp->content);
+		tmp = tmp->next;
+		j++;
+	}
+	return (NULL);
 }
 void ft_arrow_up(t_shell *shell)
 {
-    tputs(restore_cursor, 1, putchar_term);
-    tputs(tgetstr("ce", NULL), 1, putchar_term);
-    ft_putstr("\n");
-}
-void ft_arrow_down(t_shell *shell)
-{
-    tputs(restore_cursor, 1, putchar_term);
-    tputs(tgetstr("ce", NULL), 1, putchar_term);
-    write(1, "next", 5);
-}
-void	add_hist_list(t_hist **hist, t_hist *tmp)
-{
-	if (*hist)
+	if (shell->col != 0)
 	{
-		tmp->next = *hist;
-		(*hist)->prev = tmp;
-		*hist = tmp;
+		shell->col--;
+		tputs(restore_cursor, 1, putchar_term);
+		write(1, tgetstr("ce", NULL), strlen(tgetstr("ce", NULL)));
+		shell->line = line_hist(shell);
 	}
-	else
-		*hist = tmp;
-}
-void add_to_history(t_shell *shell, char *line)
-{
-	t_hist *tmp;
-
-	if(!line)
-		return ;
-	tmp = (t_hist *)malloc(sizeof(t_hist));
-	if (!tmp)
-		return ;
-	tmp->line = ft_strdup(line);
-	add_hist_list(&shell->hist, tmp);
 }
 
-void ft_arrow_enter(t_shell *shell, int i)
+void	ft_arrow_down(t_shell *shell)
 {
-    shell->line[i] = '\0';
-    ft_putstr("\n");
-    shell->col++;
-    add_to_history(shell, shell->line);
-    ft_prompt();
+	if (shell->col < shell->new_col)
+	{
+		shell->col++;
+		tputs(restore_cursor, 1, putchar_term);
+		write(1, tgetstr("ce", NULL), strlen(tgetstr("ce", NULL)));
+		shell->line = line_hist(shell);
+	}
 }
-void read_line(t_shell *shell)
-{
-    int c;
-    int i;
 
-    i = 0;
-    while (read(0, &c, sizeof(c)) >= 0)
-    {
-        if (c == CTRLD)
-            ft_ctrld();
-        else if (c == UP_ARROW)
-            ft_arrow_up(shell);
-        else if (c == DOWN_ARROW)
-            ft_arrow_down(shell);
-        else if (c == ENTER_ARROW)
-        {
-            ft_arrow_enter(shell, i);
-            i = 0;
-        }
-        else
-        {
-            shell->line[i] = c;
-            i++;
-            write(1, &c, 1);
-        }
-        c = 0;
-    }
+void	add_to_history(t_shell *shell)
+{
+	t_list *tmp;
+
+	tmp = shell->hist;
+	if (strcmp(shell->line, ""))
+	{
+		if(shell->hist == NULL)
+			shell->hist = ft_lstnew(ft_strdup(shell->line));
+		else
+		{
+			ft_lstadd_back(&shell->hist, ft_lstnew(ft_strdup(shell->line)));
+		}
+	}
+
 }
-void ft_minishell(t_shell *shell)
+void	ft_arrow_enter(t_shell *shell, int i)
 {
-    struct termios term;
-    char *term_name;
-    int c;
-    int i;
+	shell->line[i] = '\0';
+	shell->col++;
+	shell->new_col = shell->col;
+	add_to_history(shell);
+	ft_putstr("\n");
+	ft_prompt();
+	tputs(save_cursor, 1, putchar_term);
+}
 
-    term_name = getenv("TERM");
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag &= ~ICANON;
-    term.c_lflag &= ~ECHO;
+void	delete_end(t_shell *shell, int *col, int *row, char *cm, char *ce)
+{
+	if (*col != 0 && *col > 11)
+		--(*col);
+	tputs(tgoto(cm, *col, *row), 1, putchar_term);
+	tputs(ce, 1, putchar_term);
+	shell->line = ft_substr(shell->line, 0, ft_strlen(shell->line) - 1);
+}
+void	read_line(t_shell *shell)
+{
+	int	c;
+	int	i;
+
+	i = 0;
+	char *cm = tgetstr("cm", NULL); //cursor motion
+	char *ce = tgetstr("ce", NULL); //clear line from cursor
+	int row;
+	int col;
+	while (read(0, &c, sizeof(c)) >= 0)
+	{
+		get_cursor_position(&col, &row);
+		if (c == CTRLD && i == 0)
+			ft_ctrld(shell);
+		if (c == UP_ARROW)
+			ft_arrow_up(shell);
+		else if (c == DOWN_ARROW)
+			ft_arrow_down(shell);
+		else if (c == ENTER_ARROW)
+		{
+			ft_arrow_enter(shell, i);
+			i = 0;
+		}
+		else if (c == BACKSPACE)
+			delete_end(shell, &col, &row, cm, ce);
+		else
+		{
+			shell->line[i++] = c;
+			write(1, &c, 1);
+		}
+		c = 0;
+	}
+}
+
+void	ft_minishell(t_shell *shell)
+{
+	struct termios	term;
+	char			*term_name;
+	int				c;
+	int				i;
+
+	term_name = getenv("TERM");
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ICANON;
+	term.c_lflag &= ~ECHO;
 	term.c_cc[VMIN] = 1;
 	term.c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    tgetent(0, term_name); 
-    tputs(save_cursor, 1, putchar_term);
-    read_line(shell);
+	tgetent(0, term_name);
+	read_line(shell);
 }
-void ft_initialise(t_shell *shell, char **envp)
+
+void	ft_initialise(t_shell *shell, char **envp)
 {
-    shell->envp = envp;
-    shell->cmd = NULL;
-    shell->hist = NULL;
-    shell->line = (char *)malloc(sizeof(char));
-    shell->col = 0;
+	shell->envp = envp;
+	shell->cmd = NULL;
+	shell->hist = NULL;
+	shell->line = (char *)malloc(sizeof(char));
+	shell->col = 0;
+	shell->new_col = 0;
+	shell->fork = 0;
 }
-int main(int ac, char **av, char **envp)
+
+int 	main(int ac, char **av, char **envp)
 {
-    t_shell shell;
-    ft_initialise(&shell, envp);
-    signal(SIGINT, signal_handler);
-    signal(SIGQUIT, signal_handler);
-    ft_prompt();
-    ft_minishell(&shell);
-    return (0);
+	t_shell		shell;
+
+	ft_initialise(&shell, envp);
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, signal_handler);
+	ft_prompt();
+	ft_minishell(&shell);
+	return (0);
+
 }
